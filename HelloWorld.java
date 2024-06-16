f485b6001dea4bb624b80df0ea7fa31a5a3dda0e
rankaarista <amranka@wisc.edu>
2024-06-16 13:48:55 -0500

    added a for loop to test modification

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@
 public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		for (int i = 1; i <6; i -= -1){
+			System.out.print(i + " ");
+		}
+		System.out.println();
 	}
 }
